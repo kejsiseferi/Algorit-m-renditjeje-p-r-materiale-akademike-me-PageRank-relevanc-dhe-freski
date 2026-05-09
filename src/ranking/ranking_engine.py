0c0e04525c497b2f1316d66f3028bab99282e288
kejsiseferi <kejsiseferi16@gmail.com>
2026-05-09 17:53:49 +0200

    Update ranking_engine.py

--- src/ranking/ranking_engine.py
+++ src/ranking/ranking_engine.py
@@ -1,1 +1,32 @@
+import numpy as np
 
+
+def combine_scores(
+    pagerank,
+    relevance,
+    freshness,
+    quality,
+    wP=0.4,
+    wR=0.3,
+    wF=0.2,
+    wQ=0.1
+):
+
+    final_scores = []
+
+    pagerank_values = np.array(
+        list(pagerank.values())
+    )
+
+    for i in range(len(relevance)):
+
+        score = (
+            wP * pagerank_values[i]
+            + wR * relevance[i]
+            + wF * freshness[i]
+            + wQ * quality[i]
+        )
+
+        final_scores.append(score)
+
+    return np.array(final_scores)
